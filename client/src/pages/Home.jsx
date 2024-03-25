816fe722a4f4a666e153bbb251e0caccc9b23459
tid213 <tid2136141@gmail.com>
2024-03-25 16:52:51 -0700

    added bg color and button color to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,13 +12,13 @@ function Home () {
       }
 
       return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+        <div className="min-h-screen flex flex-col justify-center items-center bg-lime-100">
           <h1 className="text-4xl font-bold mb-8 text-center">Welcome to GardenNotes.me</h1>
           <div className="flex space-x-4">
-            <Link to="/signin" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            <Link to="/signin" className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded">
               Sign In
             </Link>
-            <Link to="/signup" className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+            <Link to="/signup" className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded">
               Sign Up
             </Link>
           </div>
