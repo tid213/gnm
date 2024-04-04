b8f81b122cf485ac5fb7e9f7e2c48587a5a236c3
tid213 <tid2136141@gmail.com>
2024-04-03 17:08:57 -0700

    added info section and added divs and information, added icons for info divs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import plantHouse from '../images/plant-house.svg';
 import landingImage from '../images/landing-image.svg';
-import headerImage from '../images/note-with-pencil.svg'
+import noteAndPencil from '../images/note-with-pencil.svg'
+import plantIcon from '../images/plant-icon.svg';
+import imgIcon from '../images/img-icon.svg';
+import weatherIcon from '../images/weather-icon.svg';
 
 function Home () {
 
@@ -71,21 +74,15 @@ function Home () {
             </header>
             <section className=" min-h-screen flex flex-col mt-16">
                 <div className="lg:flex lg:items-center items-center">
-                    {/* Image */}
                     <div className="lg:w-6/12">
                         <img className="mb-4 lg:mt-4 lg:mb-0 lg:mr-8  max-w-sm mx-auto lg:max-w-lg" src={landingImage} alt="Landing Image" />
                     </div>
-                    {/* Text content */}
                     <div className="lg:w-6/12">
-                    {/* Heading */}
                     <p className="inter font-normal mb-4 max-w-prose px-8 text-2xl text-customDarkGreen text-center ">Welcome to</p>
                     <h1 className="text-5xl text-customDarkGreen inter font-bold mb-4 lg:mb-8 text-center">gardennotes<b className="text-customOrange">.</b><b className="text-customMidGreen">me</b></h1>
-                    
-                    {/* Paragraph */}
                     <p className="inter font-light max-w-prose px-8 text-lg text-black text-center ">
                     Your all-in-one solution for plant lovers. Whether you're a seasoned gardener or just starting out, gardennotes.me helps you keep track of all your plants effortlessly.
                     </p>
-                    {/* Buttons */}
                     <div className="flex flex-col w-full lg:justify-center items-center mt-6 lg:flex-row">
                         <Link to="/signin" className="inline-block inter bg-customMidGreen w-8/12 lg:w-3/12 hover:bg-customDarkGreen text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign In</Link>
                         <Link to="/signup" className="inline-block inter mt-2 lg:mt-0 lg:ml-4 bg-customOrange w-8/12 lg:w-3/12 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign Up</Link>
@@ -93,11 +90,40 @@ function Home () {
                     </div>
                 </div>
             </section>
-            <section className="flex space-x-4">
-            <p className="inter font-light max-w-prose px-8 text-lg text-black text-center ">
-                <b className="font-bold">Take Notes:</b> Capture your plant care routines, growth milestones, and observations in one convenient place. With GreenNotes, you'll never forget a watering schedule or pruning tip again
-            </p>
-            </section>
+            <section className="container mx-auto py-8">
+                {/* Top row */}
+                <div className="flex justify-center">
+                    <div className="text-center"><h1 className="text-5xl text-customDarkGreen inter font-bold mb-4 lg:mb-8 text-center">here to help<b className="text-customOrange">.</b></h1></div>
+                </div>
+                <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 lg:mx-24 mx-4">
+                    <div className="bg-white p-4 flex items-center rounded-lg shadow-md">
+                    <img src={noteAndPencil} alt="Note and Pencil" className="mr-4 max-h-8" />
+                    <p className="inter font-light max-w-prose text-lg text-black text-left">
+                        <b className="font-bold">Take Notes:</b> Capture your plant care routines, growth milestones, and observations in one convenient place. With GreenNotes, you'll never forget a watering schedule or pruning tip again
+                    </p>
+                    </div>
+                    <div className="bg-white p-4 flex items-center rounded-lg shadow-md">
+                    <img src={plantIcon} alt="Note and Pencil" className="mr-4 max-h-8" />
+                    <p className="inter font-light max-w-prose text-lg text-black text-left">
+                        <b className="font-bold">Track Your Plants:</b> Organize and manage your plant collection with ease. Keep track of each plant's species, location, and important details to ensure they thrive.
+                    </p>
+                    </div>
+                    <div className="bg-white p-4 flex items-center rounded-lg shadow-md">
+                    <img src={imgIcon} alt="Note and Pencil" className="mr-4 max-h-8" />
+                    <p className="inter font-light max-w-prose text-lg text-black text-left">
+                        <b className="font-bold">Upload Pictures:</b> Share your plant journey visually by uploading photos of your plants. Document their growth progress and showcase your green thumb skills to friends and fellow plant enthusiasts.
+                    </p>
+                    </div>
+                    <div className="bg-white p-4 flex items-center rounded-lg shadow-md">
+                    <img src={weatherIcon} alt="Note and Pencil" className="mr-4 max-h-8" />
+                    <p className="inter font-light max-w-prose text-lg text-black text-left">
+                        <b className="font-bold">Weather tracking:</b> Provides real-time local weather updates, including temperature and precipitation forecasts, to help you make informed decisions about watering, planting, and caring for your plants, ensuring they thrive in every season.
+                    </p>
+                    </div>
+                </div>
+
+                {/* Bottom row */}
+                </section>
         </main>
       );
 
