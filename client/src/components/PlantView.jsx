96c544af3e353920b4492610b6866cf0f734cee8
tid213 <tid2136141@gmail.com>
2024-04-10 18:28:30 -0700

    changed view plant cols

--- client/src/components/PlantView.jsx
+++ client/src/components/PlantView.jsx
@@ -27,19 +27,20 @@ function PlantView({session, plantID, closeButton}){
     }, [])
 
     return(
-        <div className='relative w-full inter mt-12 max-w-sm md:max-w-md mx-auto p-6 bg-white rounded-lg shadow-md'>
-            <div onClick={()=> closeButton(true)} className='absolute text-xl font-bold right-4 top-2 cursor-pointer'><a>X</a></div>
+        <div>
             {plantData ? 
+                <div className='relative w-full inter mt-12 max-w-sm md:max-w-md mx-auto p-6 bg-white rounded-lg shadow-md'>
+                <div onClick={()=> closeButton(true)} className='absolute text-xl font-bold right-4 top-2 cursor-pointer'><a>X</a></div>
                 <div className='grid grid-cols-4 grid-flow-row gap-4'>
-                    <div className='col-span-4'><p>{plantData[0].plant_name}</p></div>
-                    <div className='col-span-4'><p>{plantData[0].plant_plot}</p></div>
-                    <div className='col-span-4'>{plantData[0].plant_image ? 
-                                                <img src={plantData[0].plant_image}></img>:
-                                                <img src={tempImage}></img>}
-                    </div>
-                    <div className='col-span-4'>EDIT DELETE</div>
-                </div> 
-            : ""}
+                        <div className='col-span-4'><h1 className="text-2xl font-bold inter text-customBrown">{plantData[0].plant_name}<b className="text-customOrange">.</b><b className="text-customMidGreen text-xl font-normal">plot/{plantData[0].plant_plot}</b></h1></div>
+                        <div className='col-span-4 '>{plantData[0].plant_image ? 
+                                                    <img src={plantData[0].plant_image}></img>:
+                                                    <img src={tempImage}></img>}
+                        </div>
+                        <div className='col-span-2'><p>{plantData[0].prune_date}<br/>{plantData[0].prune_freq}</p></div>
+                        <div className='col-span-4'>EDIT DELETE</div>
+                </div>
+            </div>: ""}
         </div>
         )
 }
