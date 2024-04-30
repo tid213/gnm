8711809cb35bf2545f5775eb085e1a3056d117c7
tid213 <tid2136141@gmail.com>
2024-04-29 17:10:19 -0700

    added images to view note

--- client/src/components/NoteView.jsx
+++ client/src/components/NoteView.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
 import closeImage from '../images/x.svg';
 import editImage from '../images/edit.svg'
+import trashImage from '../images/trash.svg';
 
 function NoteView({noteID, closeButton, editButton}){
 
@@ -38,7 +39,7 @@ function NoteView({noteID, closeButton, editButton}){
             <div className='relative w-full inter mt-8 lg:mt-8 lg:w-96 max-w-sm md:max-w-md mx-auto p-6 border border-gray-200 rounded-lg shadow-md bg-white'>
             <div onClick={()=> closeButton(true)} className='absolute text-xl font-bold right-4 top-2 cursor-pointer'><img src={closeImage} className='h-4 w-4 '></img></div>
             <div className='lg:grid lg:grid-col-2 gap-4'>
-                <div className='col-span-2 text-xl'>
+                <div className='col-span-2 text-xl font-semibold'>
                     <h3>{noteData[0].note_type === "Garden" ? noteData[0].note_type : noteData[0].note_for}</h3>
                 </div>
                 <div className='col-span-2'>
@@ -46,9 +47,14 @@ function NoteView({noteID, closeButton, editButton}){
                 </div>
             </div>
             <div onClick={()=>editButton("plant")}
-                className='lg:w-fit w-full m-auto lg:mt-1 lg:p-0 p-1 mt-1 bg-white lg:bg-transparent w-auto cursor-pointer flex justify-center items-center border lg:border-0 hover:border-b-2 lg:shadow-none shadow-md'>
+                className='lg:w-fit col-span-1 w-full m-auto lg:mt-4 lg:p-0 p-1 mt-1 bg-white lg:bg-transparent w-auto cursor-pointer flex justify-center items-center border lg:border-0 hover:border-b-2 lg:shadow-none shadow-md'>
                 <img src={editImage} className='w-4 h-4'></img>
                 <p className='text-normal font-normal text-black p-2'>Edit</p>
+            </div>
+            <div onClick={()=>editButton("plant")}
+                className='lg:w-fit col-span-1 w-full m-auto lg:mt-1 lg:p-0 p-1 mt-1 bg-white lg:bg-transparent w-auto cursor-pointer flex justify-center items-center border lg:border-0 hover:border-b-2 lg:shadow-none shadow-md'>
+                <img src={trashImage} className='w-4 h-4'></img>
+                <p className='text-normal font-normal text-black p-2'>Delete</p>
             </div>
             </div> : ""}
         </div>
