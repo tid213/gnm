57e01d81b00b680a1a6008ae9f22f3a6c4c19637
tid213 <tid2136141@gmail.com>
2024-05-01 16:22:15 -0700

    added package json to root folder

--- server/server.js
+++ server/server.js
@@ -162,6 +162,6 @@ app.get('/notebook/:id', async (req, res) => {
 
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running on port 8000.`);
   });
